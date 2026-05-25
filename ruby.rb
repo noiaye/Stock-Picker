3d28f66513bf42c27198f2a0b20da4a259b36d53
blob <130174758+BlobCybersecurity@users.noreply.github.com>
2026-05-25 18:27:00 +0200

    Comment logic

--- ruby.rb
+++ ruby.rb
@@ -1,60 +1,24 @@
 def stock_picker(array)
   storageHAsh = {}
-  biggest = {0 => [0]}
-  print(biggest[0])
-  array.each_with_index do |v, i|
-    p "I is: ", i
-   
-  
-        array.each do |elm| 
-          p "elm is ", elm
-          if elm > v  && array.find_index(elm) > i
-            tracker = 0
-            array.each do |tracks|
-              if tracks > tracker && tracks > elm 
-                tracker = tracks
-              end
-            end
-            elm = tracker
 
-          end
-          storageHAsh[elm] = [v-elm]
-
-            
-          
-          # if elm > v && array.find_index(elm) > i
-            
-          #   storageHAsh[elm] = "Element: #{elm} - #{v} = #{elm-v}"
-           
-          # end
-        end
-    
-          
-    
-
-    
-  end
-
-  biggest = storageHAsh.max_by {|k, v| v}
-  
-  return biggest
-
-
-    
-
-
-
-  
 
 end
 
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
 
-## If the largest number is first then we skip to the next iteration, 
-## If the current number is the last number then end the loop
-## Take the current number, and store it somwehere, 
-## Take the largest number that is after it and store it
-# Store their diffences in a hash 
-#filter the hash for the largest difference
-#output that value to the array
+## If the current index is 0 and the value is the biggest value 
+## then we go to the next index 
+## then we start the logic:
+#
+#---------------- do we really need the above?
+## Take the current item, and look at doing the operation (the current item - loop item)
+## We also create a variable for storing the smallest value
+#If the value is smaller than smallest value
+#it becomes the new smallest value
+## Now after the loop is finished, the variable should be the smallest sum for this current index
+## So in order to store this value 
+## We take a hash, and its values would be {current value, the element which when you do the operation current value - element gets the smallest variable we just got}
+## Now we have hashes for all the elements, and we simply filter for the smallest element
+## Then we look at the value for the hash and return its values but instead, the indexes of these values 
+## boom
